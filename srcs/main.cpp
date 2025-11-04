bdd82037f2818d96a49d9d3cfb1f5745411a4bd3
julcalde <julcalde@student.42heilbronn.de>
2025-11-04 18:11:18 +0100

    [main.cpp] test without run() method for parsing check. Output error message is correct.

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,24 @@
-#include "Server.hpp"
+#include "../include/Server.hpp"
 
 int main(int argc, char **argv)
 {
-
+	if (argc != 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+		return (1);
+	}
+	
+	try
+	{
+		int port = std::atoi(argv[1]);
+		std::string password = argv[2];
+		Server server(port, password);
+		// server.run(); // Uncomment this line when the run method is implemented
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return (1);
+	}
+	return (0);
 }
